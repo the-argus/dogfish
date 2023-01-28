1aed8b71d7c399eacd948b3e624502225246a589
Ian McFarlane <i.mcfarlane2002@gmail.com>
2023-01-28 17:52:21 -0500

    init window

---
+++ src/include/constants.h
@@ -0,0 +1,4 @@
+#pragma once
+
+#define WINDOW_WIDTH 720
+#define WINDOW_HEIGHT 480
--- src/main.c
+++ src/main.c
@@ -1,7 +1,25 @@
 #include <stdio.h>
+#include "raylib.h"
+#include "constants.h"
 
 int main(void)
 {
-	printf("lungfish...");
+    char window_open = 1;
+    InitWindow(WINDOW_WIDTH, WINDOW_HEIGHT, "lungfish");
+
+    SetTargetFPS(60);
+
+	printf("lungfish...\n");
+
+    while (window_open) {
+        BeginDrawing();
+
+        ClearBackground(RAYWHITE);
+
+        EndDrawing();
+    }
+
+    CloseWindow();
+
 	return 0;
 }
