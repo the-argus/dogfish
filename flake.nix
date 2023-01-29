bb8f97aaf2104eb5aac3d888ea52983a3cd5860a
Ian McFarlane <i.mcfarlane2002@gmail.com>
2023-01-29 01:46:26 -0500

    try lungfish musl

--- flake.nix
+++ flake.nix
@@ -11,8 +11,73 @@
     ...
   }: let
     system = "x86_64-linux";
-    pkgs = import nixpkgs {inherit system;};
+    pkgs = import nixpkgs {localSystem = {inherit system;};};
+    regularPkgs = pkgs;
+    clangMuslPkgs = import nixpkgs {
+      localSystem = {
+        inherit system;
+        libc = "musl";
+        config = "x86_64-unknown-linux-musl";
+      };
+      config.replaceStdenv = {pkgs, ...}:
+        pkgs.clangStdenv.override {
+          cc = regularPkgs.buildPackages.clang;
+        };
+      # make lungfish's build system and some deps come from standard packages
+      overlays = [
+        (_: _: {
+          cmake = pkgs.cmake;
+          pkg-config = pkgs.pkg-config;
+          raylib-games = pkgs.emptyDirectory;
+          gfortran = pkgs.gfortran;
+          libpulseaudio = pkgs.libpulseaudio;
+          mesa = pkgs.mesa;
+          gettext = pkgs.gettext;
+        })
+        (_: _: {
+          ninja = pkgs.ninja.override {python3 = pkgs.python3Minimal;};
+        })
+        (_: _: {
+          meson = pkgs.meson.override {python3 = pkgs.python3Minimal;};
+        })
+        (_: super: {
+          raylib = super.raylib.override {sharedLib = false;};
+        })
+      ];
+    };
+
+    lungfish = {
+      stdenv,
+      ninja,
+      pkg-config,
+      raylib,
+      meson,
+      ...
+    }:
+      stdenv.mkDerivation {
+        name = "lungfish";
+        src = ./src;
+        nativeBuildInputs = [
+          pkg-config
+          raylib
+          meson
+          ninja
+        ];
+        buildPhase = ''
+          meson setup builddir
+          meson compile -C builddir
+        '';
+        installPhase = ''
+          mkdir -p $out/bin
+          mv builddir/lungfish $out/bin/lungfish
+        '';
+      };
   in {
+    packages.${system} = {
+      lungfishMusl = clangMuslPkgs.callPackage lungfish {};
+      lungfish = pkgs.callPackage lungfish {};
+      default = lungfish;
+    };
     devShell.${system} =
       pkgs.mkShell.override
       {
