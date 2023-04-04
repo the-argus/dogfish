0adbfad8013737793dbbd0673ccf9e3e68f67512
Ian McFarlane <i.mcfarlane2002@gmail.com>
2023-04-04 12:19:58 -0400

    refactor main function

--- src/include/architecture.h
+++ src/include/architecture.h
@@ -2,27 +2,27 @@
 #include "raylib.h"
 #include "fps_camera.h"
 
-struct Mousestate {
+typedef struct Mousestate {
     unsigned char left_pressed;
     unsigned char right_pressed;
     Vector2 position;
     Vector2 virtual_position;
-};
+} Mousestate;
 
-struct Keystate {
+typedef struct Keystate {
     unsigned char up;
     unsigned char left;
     unsigned char right;
     unsigned char down;
-};
+} Keystate;
 
-struct Inputstate {
+typedef struct Inputstate {
     struct Mousestate mouse;
     struct Keystate keys;
-};
+} Inputstate;
 
-struct Gamestate {
+typedef struct Gamestate {
     struct Inputstate input;
     Camera *current_camera;
     CameraData camera_data;
-};
+} Gamestate;
--- src/main.c
+++ src/main.c
@@ -12,95 +12,135 @@
 #define MAX(a, b) ((a) > (b) ? (a) : (b))
 #define MIN(a, b) ((a) < (b) ? (a) : (b))
 
+static Gamestate gamestate;
+static float scale;
+static RenderTexture2D main_target;
+
+void window_settings();
+void init_rendertexture();
+void gather_input();
+void update();
+void main_draw();
+void window_draw();
+
 int main(void)
 {
-	bool window_open = true;
+	// set windowing backend vars like title and size
+	window_settings();
+	// initialize main_texture to the correct size
+	init_rendertexture();
 
-	SetTargetFPS(60);
-	SetConfigFlags(FLAG_WINDOW_RESIZABLE | FLAG_VSYNC_HINT);
-	InitWindow(WINDOW_WIDTH, WINDOW_HEIGHT, "lungfish");
-	SetWindowMinSize(WINDOW_WIDTH, WINDOW_HEIGHT);
-
-	// variable width screen
-	float scale;
-	RenderTexture2D main_target = LoadRenderTexture(GAME_WIDTH, GAME_HEIGHT);
-	SetTextureFilter(main_target.texture, TEXTURE_FILTER_BILINEAR);
-
-	// inialize gamestate
-	struct Gamestate *gamestate;
-	gamestate = malloc(sizeof(struct Gamestate));
-	if (gamestate == NULL) {
-		printf("Malloc failed\n");
-		return 1;
-	}
-
-	// init some of the gamestate values
-	gamestate->input.mouse.virtual_position = (Vector2){0};
-	gamestate_new_fps_camera(gamestate);
+	// inialize gamestate struct
+	gamestate.input.mouse.virtual_position = (Vector2){0};
+    // this initializes current_camera, which involves a malloc
+	gamestate_new_fps_camera(&gamestate);
 
 	// initialization complete
 	printf("lungfish...\n");
 
+	// loop until player presses escape or close button
+	bool window_open = true;
 	while (window_open) {
-		// find what window resizing is necessary
-		scale = MIN((float)GetScreenWidth() / GAME_WIDTH,
-					(float)GetScreenHeight() / GAME_HEIGHT);
-
-		// collect mouse information
-		gamestate->input.mouse.position = GetMousePosition();
-		set_virtual_mouse_position(gamestate, scale);
-
-		gamestate->input.mouse.left_pressed =
-			IsMouseButtonPressed(MOUSE_BUTTON_LEFT);
-		gamestate->input.mouse.right_pressed =
-			IsMouseButtonPressed(MOUSE_BUTTON_RIGHT);
-
-		// collect keyboard information
-		gamestate->input.keys.right = IsKeyDown(KEY_RIGHT) || IsKeyDown(KEY_D);
-		gamestate->input.keys.left = IsKeyDown(KEY_LEFT) || IsKeyDown(KEY_A);
-		gamestate->input.keys.up = IsKeyDown(KEY_UP) || IsKeyDown(KEY_W);
-		gamestate->input.keys.down = IsKeyDown(KEY_DOWN) || IsKeyDown(KEY_S);
-
 		if (IsKeyPressed(KEY_ESCAPE) || WindowShouldClose()) {
 			window_open = false;
 		}
 
-        FpsCameraUpdate(gamestate->current_camera, &(gamestate->camera_data));
+		// fraction of window resize that will occur this frame, basically the
+		// difference between current width/height ratio to target width/height
+		scale = MIN((float)GetScreenWidth() / GAME_WIDTH,
+					(float)GetScreenHeight() / GAME_HEIGHT);
 
-		// draw to render texture
+		// set the variables in gamestate to reflect input state
+		gather_input();
+
+		// update in-game elements before drawing
+		update();
+
+		// set draw target to the rendertexture, dont actually draw to window
 		BeginTextureMode(main_target);
-		ClearBackground(RAYWHITE);
-		BeginMode3D(*gamestate->current_camera);
-        // draw a cube and then look at it
-		DrawCube((Vector3){0}, 0.2f, 0.2f, 0.2f, RED);
-        gamestate->current_camera->target = (Vector3){0};
-        gamestate->current_camera->position = (Vector3){1, 1, 1};
+		// clang-format off
+		    ClearBackground(RAYWHITE);
+            BeginMode3D(*gamestate.current_camera);
+                // draw in-game objects
+                main_draw();
 
-        // grid for visual aid
-        DrawGrid(10, 1.0f);
-		EndMode3D();
+            EndMode3D();
+		// clang-format on
 		EndTextureMode();
-
+        
+        // draw the game to the window at the correct size
 		BeginDrawing();
-		// color of the bars around the rendertexture
-		ClearBackground(BLACK);
-		// draw the render texture scaled
-		DrawTexturePro(
-			main_target.texture,
-			(Rectangle){0.0f, 0.0f, (float)main_target.texture.width,
-						(float)-main_target.texture.height},
-			(Rectangle){(GetScreenWidth() - ((float)GAME_WIDTH * scale)) * 0.5f,
-						(GetScreenHeight() - ((float)GAME_HEIGHT * scale)) *
-							0.5f,
-						(float)GAME_WIDTH * scale, (float)GAME_HEIGHT * scale},
-			(Vector2){0, 0}, 0.0f, WHITE);
+		window_draw();
 		EndDrawing();
 	}
 
 	// cleanup
-	free(gamestate->current_camera);
-	free(gamestate);
+	free(gamestate.current_camera);
 	CloseWindow();
 
 	return 0;
+}
+
+void update()
+{
+	FpsCameraUpdate(gamestate.current_camera, &(gamestate.camera_data));
+}
+
+void main_draw()
+{
+	// draw a cube and then look at it
+	DrawCube((Vector3){0}, 0.2f, 0.2f, 0.2f, RED);
+	gamestate.current_camera->target = (Vector3){0};
+	gamestate.current_camera->position = (Vector3){1, 1, 1};
+
+	// grid for visual aid
+	DrawGrid(10, 1.0f);
+}
+
+void window_draw()
+{
+	// color of the bars around the rendertexture
+	ClearBackground(BLACK);
+	// draw the render texture scaled
+	DrawTexturePro(
+		main_target.texture,
+		(Rectangle){0.0f, 0.0f, (float)main_target.texture.width,
+					(float)-main_target.texture.height},
+		(Rectangle){(GetScreenWidth() - ((float)GAME_WIDTH * scale)) * 0.5f,
+					(GetScreenHeight() - ((float)GAME_HEIGHT * scale)) * 0.5f,
+					(float)GAME_WIDTH * scale, (float)GAME_HEIGHT * scale},
+		(Vector2){0, 0}, 0.0f, WHITE);
+}
+
+void window_settings()
+{
+	SetTargetFPS(60);
+	SetConfigFlags(FLAG_WINDOW_RESIZABLE | FLAG_VSYNC_HINT);
+	InitWindow(WINDOW_WIDTH, WINDOW_HEIGHT, "lungfish");
+	SetWindowMinSize(WINDOW_WIDTH, WINDOW_HEIGHT);
+}
+
+void init_rendertexture()
+{
+	// variable width screen
+	main_target = LoadRenderTexture(GAME_WIDTH, GAME_HEIGHT);
+	SetTextureFilter(main_target.texture, TEXTURE_FILTER_BILINEAR);
+}
+
+void gather_input()
+{
+	// collect mouse information
+	gamestate.input.mouse.position = GetMousePosition();
+	set_virtual_mouse_position(&gamestate, scale);
+
+	gamestate.input.mouse.left_pressed =
+		IsMouseButtonPressed(MOUSE_BUTTON_LEFT);
+	gamestate.input.mouse.right_pressed =
+		IsMouseButtonPressed(MOUSE_BUTTON_RIGHT);
+
+	// collect keyboard information
+	gamestate.input.keys.right = IsKeyDown(KEY_RIGHT) || IsKeyDown(KEY_D);
+	gamestate.input.keys.left = IsKeyDown(KEY_LEFT) || IsKeyDown(KEY_A);
+	gamestate.input.keys.up = IsKeyDown(KEY_UP) || IsKeyDown(KEY_W);
+	gamestate.input.keys.down = IsKeyDown(KEY_DOWN) || IsKeyDown(KEY_S);
 }
