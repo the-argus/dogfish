03be4734841f1547ef344da5955ea0ca3e108c29
Ian McFarlane <i.mcfarlane2002@gmail.com>
2023-04-06 22:34:34 -0400

    gameobject architecture

--- src/include/architecture.h
+++ src/include/architecture.h
@@ -1,5 +1,12 @@
 #pragma once
 #include "raylib.h"
+#include "ode/ode.h"
+
+#define OPTIONAL(type) \
+typedef struct Opt_##type { \
+    unsigned char has; \
+    type func; \
+} Opt_##type;
 
 typedef struct
 {
@@ -32,3 +39,22 @@ typedef struct Gamestate {
     Camera *current_camera;
     CameraData camera_data;
 } Gamestate;
+
+struct GameObject;
+
+typedef void (*UpdateFunction)(Gamestate *gamestate, float delta_time);
+typedef void (*DrawFunction)(Gamestate *gamestate);
+
+// versions of types but they have a true/false for whether or not they
+// are valid/populated
+typedef dBodyID PhysicsBody;
+OPTIONAL(UpdateFunction)
+OPTIONAL(DrawFunction)
+OPTIONAL(PhysicsBody)
+
+// THE game object!!!!
+typedef struct GameObject {
+    Opt_PhysicsBody body;
+    Opt_DrawFunction draw;
+    Opt_UpdateFunction update;
+} GameObject;
