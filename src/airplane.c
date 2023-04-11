7f8ec04eed0ab71e21ac430fb1ff64370d55eecc
Will O'Brien <56482432+UsefulGitHub@users.noreply.github.com>
2023-04-10 22:06:14 -0400

    Player Index Assigns Update Method

--- src/airplane.c
+++ src/airplane.c
@@ -3,27 +3,35 @@
 // Like a constructor:
 // Hook up all the GameObject optional features
 // Attach the chosen inputs to the update method
+// player = 0 for p1, 1 for p2
 void setup_airplane(GameObject *plane, Gamestate *gamestate,
 					ControllerState *controller, Cursorstate *cursor,
-					Keystate *keys)
+					Keystate *keys, unsigned int player)
 {
-    // Set up the values for the opts
-    plane->draw.value = &airplane_draw;
-    plane->update.value = &airplane_update;
+	// Set up the values for the opts
+	plane->draw.value = &airplane_draw;
+	if (player == 0) {
+		plane->update.value = &airplane_update_p1;
+	}
+    else {
+        plane->update.value = &airplane_update_p2;
+    }
 
-    // Say that it has them
-    plane->draw.has = 1;
-    plane->update.has = 1;
+	// Say that it has them
+	plane->draw.has = 1;
+	plane->update.has = 1;
 }
 
 // Accept input state and move accordingly
-void airplane_update(struct GameObject *self, Gamestate *gamestate, float delta_time)
+void airplane_update_p1(struct GameObject *self, Gamestate *gamestate,
+						float delta_time)
 {
 }
 
-void airplane_update_p2(struct GameObject *self, Gamestate *gamestate, float delta_time)
+void airplane_update_p2(struct GameObject *self, Gamestate *gamestate,
+						float delta_time)
 {
-    gamestate->input.controller_2;
+	gamestate->input.controller_2;
 }
 
 // Draw the plane
--- src/airplane.h
+++ src/airplane.h
@@ -5,13 +5,14 @@
 // Like a constructor:
 // Hook up all the GameObject optional features
 // Attach the chosen inputs to the update method
-void setup_airplane(GameObject *plane, 
-					ControllerState *controller, Cursorstate *cursor,
-					Keystate *keys);
+void setup_airplane(GameObject *plane, Gamestate *gamestate, ControllerState *controller,
+					Cursorstate *cursor, Keystate *keys, unsigned int player);
 
-// Accept input state and move accordingly
-void airplane_update(ControllerState *controller, Cursorstate *cursor,
-					 Keystate *keys);
+// Accept p1's input state from game state, and move accordingly
+void airplane_update_p1(struct GameObject *self, Gamestate *gamestate, float delta_time);
+
+// Accept p2's input state from game state, and move accordingly
+void airplane_update_p2(struct GameObject *self, Gamestate *gamestate, float delta_time);
 
 // Draw the plane
 void airplane_draw();
