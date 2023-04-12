b3c488483d2c3d1034febe2c26cce1744be67948
Ian McFarlane <i.mcfarlane2002@gmail.com>
2023-04-12 16:19:46 -0400

    remove residual test_cube code

--- src/physics.c
+++ src/physics.c
@@ -10,20 +10,11 @@
 
 static dWorldID world;
 static dSpaceID space;
-static dGeomID ground;
-
-static dBodyID test_cube;
-static dGeomID test_cube_geom;
-static const float test_cube_mass = 1;
+static dGeomID ground; // accessible only here, exposed only by the on_ground
+					   // function
 static dJointGroupID contactgroup;
 
 Vector3 to_raylib(const dVector3 v3) { return (Vector3){v3[0], v3[1], v3[2]}; }
-const dReal *get_test_cube_position() { return dBodyGetPosition(test_cube); }
-Vector3 get_test_cube_size()
-{
-	const Vector3 size = {1, 1, 1};
-	return size;
-}
 
 static void init_contact(dContact *contact)
 {
@@ -32,7 +23,7 @@ static void init_contact(dContact *contact)
 	contact->surface.mu2 = 0;
 }
 
-static int on_ground(dBodyID body)
+int on_ground(dBodyID body)
 {
 	dGeomID geom = dBodyGetFirstGeom(body);
 	dContact contact;
@@ -45,65 +36,6 @@ static int on_ground(dBodyID body)
 		return upness > ON_GROUND_THRESHHOLD;
 	}
 	return 0;
-}
-
-void apply_player_input_impulses(Inputstate inputstate, float angle_x)
-{
-	Vector3 impulse = {0};
-	Vector2 input = total_input(inputstate, 0);
-
-	impulse.x = sin(angle_x) * PLAYER_MOVE_IMPULSE;
-	impulse.z = cos(angle_x) * PLAYER_MOVE_IMPULSE;
-	Vector3 h_impulse =
-		Vector3CrossProduct(Vector3Normalize(impulse), (Vector3){0, 1, 0});
-
-	impulse = Vector3Scale(impulse, input.y);
-
-	// also grab left/right input
-	h_impulse = Vector3Scale(h_impulse, input.x);
-
-	impulse = Vector3Add(impulse, h_impulse);
-
-	dBodyAddForce(test_cube, impulse.x, impulse.y, impulse.z);
-}
-
-void apply_airplane_input_impulses(dBodyID plane, Keystate keys, ControllerState controls)
-{
-    UNUSED(plane);
-    UNUSED(keys);
-    UNUSED(controls);
-	// Get the current linear and angular velocity
-	// dVector3 *forward = dBodyGetLinearVel(plane);
-
-	// Check the state of the stick inputs (for your player index)
-	// if up/down, apply pitch
-	// if left/right, apply roll
-
-	// Check the state of the keys (for your player index)
-	// if lb/rb, apply yaw
-
-	// Transform forward by the rotation matrix
-	// Apply force on the transformed forward vector
-
-	// Make this an impulse and apply it
-
-	// --- COPIED PLAYER MOVEMENT CODE FOR YOUR REFERENCE ---
-	// Vector3 impulse = {0};
-	// Vector2 input = total_input(inputstate, player_index);
-
-	// //impulse.x = sin(angle_x) * PLAYER_MOVE_IMPULSE;
-	// //impulse.z = cos(angle_x) * PLAYER_MOVE_IMPULSE;
-	// Vector3 h_impulse =
-	// 	Vector3CrossProduct(Vector3Normalize(impulse), (Vector3){0, 1, 0});
-
-	// impulse = Vector3Scale(impulse, input.y);
-
-	// // also grab left/right input
-	// h_impulse = Vector3Scale(h_impulse, input.x);
-
-	// impulse = Vector3Add(impulse, h_impulse);
-
-	// dBodyAddForce(test_cube, impulse.x, impulse.y, impulse.z);
 }
 
 static void nearCallback(void *data, dGeomID o1, dGeomID o2)
@@ -159,34 +91,6 @@ void init_physics(Gamestate *gamestate)
 	contactgroup = dJointGroupCreate(0);
 	dWorldSetGravity(world, 0, -GRAVITY, 0);
 	ground = dCreatePlane(space, 0, 1, 0, 0);
-
-#pragma region CUBE
-
-	// create a cube which raylib can draw later
-	test_cube = dBodyCreate(world);
-	dBodySetPosition(test_cube, 0, 5, 0);
-
-	// set up the mass
-	dMass cube_mass;
-	Vector3 cube_size = get_test_cube_size();
-	dMassSetBox(&cube_mass, 1, cube_size.x, cube_size.y, cube_size.z);
-	dMassAdjust(&cube_mass, test_cube_mass);
-
-	// apply the mass to the cube
-	dBodySetMass(test_cube, &cube_mass);
-
-	// infinite moment of inertia, basically
-	dBodySetAngularDamping(test_cube, 1);
-
-	// make geometry and apply it to the cube body
-	test_cube_geom = dCreateBox(space, cube_size.x, cube_size.y, cube_size.z);
-	dGeomSetBody(test_cube_geom, test_cube);
-
-#pragma endregion
-
-#pragma region PLANE1
-
-#pragma endregion
 
 	// allocate the data for this thread to access ODE
 	assert(dAllocateODEDataForThread(dAllocateMaskAll));
--- src/physics.h
+++ src/physics.h
@@ -8,9 +8,4 @@ Vector3 to_raylib(const dVector3 v3);
 void init_physics(Gamestate *gamestate);
 void update_physics(float delta_time);
 void close_physics();
-
-const dReal *get_test_cube_position();
-Vector3 get_test_cube_size();
-
-void apply_player_input_impulses(Inputstate inputstate, float angle_x);
-void apply_airplane_input_impulses(dBodyID plane, Keystate keys, ControllerState controls);
+int on_ground(dBodyID body);
